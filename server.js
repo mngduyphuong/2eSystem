1bb3b3adbabc2124ea49643cbdfb1bb99cbe0023
Duy Phuong Nguyen <mngduyphuong@gmail.com>
2022-06-01 14:09:59 +1000

    connect db

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/db", async (req, res) => {
     const client = await pool.connect();
     const result = await client.query("SELECT * FROM test_table");
     const results = { results: result ? result.rows : null };
-    res.json("pages/db", results);
+    res.send("pages/db", results);
     client.release();
   } catch (err) {
     console.error(err);
