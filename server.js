6341f1a0e55e4fa77806a7166bc44ddae6a555b6
Duy Phuong Nguyen <mngduyphuong@gmail.com>
2022-06-03 12:30:02 +1000

    feat: test deploy api airline country

--- server.js
+++ server.js
@@ -1,48 +1,57 @@
 const express = require("express");
-const serveStatic = require("serve-static");
+// const serveStatic = require("serve-static");
 const path = require("path");
 
 const app = express();
 
-const { Pool } = require("pg");
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-console.log(pool);
-
-//here we are configuring dist to serve app files
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
-
-// this * route is to serve project on different page routes except root `/`
-// app.get(/.*/, function (req, res) {
-//   res.sendFile(path.join(__dirname, "/dist/index.html"));
+// const { Pool } = require("pg");
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
 // });
-app.get("/db", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM test_table");
-    const results = { results: result ? result.rows : null };
-    res.send(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
+// const pool = new Pool({
+//   connectionString:
+//     process.env.DATABASE_URL || "postgresql://postgres:Teobeo25021999@localhost:5432/database",
+//   ssl: process.env.DATABASE_URL ? true : false,
+// });
 
-//get time test
-app.get("/times", (req, res) => res.send(showTimes()));
-function showTimes() {
-  let result = "";
-  const times = process.env.TIMES || 5;
-  for (let i = 0; i < times; i++) {
-    result += i + " ";
-  }
-  return result;
-};
+//Set static folder
+// app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "dist")));
+
+//Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//use api
+app.use('/api/country', require("./src/api/country"));
+app.use('/api/airline', require("./src/api/airline"));
+
+// app.get("/api/db", async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query("SELECT * FROM countries_table");
+//     const results = { results: result ? result.rows : null };
+//     res.send(results);
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Error " + err);
+//   }
+// });
+
+// //get time test
+// app.get("/times", (req, res) => res.send(showTimes()));
+// function showTimes() {
+//   let result = "";
+//   const times = process.env.TIMES || 5;
+//   for (let i = 0; i < times; i++) {
+//     result += i + " ";
+//   }
+//   return result;
+// };
 
 const port = process.env.PORT || 8080;
 app.listen(port);
---
+++ src/api/airline.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:Teobeo25021999@localhost:5432/database",
+  ssl: process.env.DATABASE_URL ? true : false,
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM airlines_table");
+    const results = { results: result ? result.rows : null };
+    res.json(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+module.exports = router;
