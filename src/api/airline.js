c5214bab65af6a034cc23d32e8bcaf85f9928595
Duy Phuong Nguyen <mngduyphuong@gmail.com>
2022-06-03 14:51:07 +1000

    feat: update, delete, create airline

--- src/api/airline.js
+++ src/api/airline.js
@@ -6,13 +6,14 @@ const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
     "postgresql://postgres:Teobeo25021999@localhost:5432/database",
-  ssl: process.env.DATABASE_URL ? true : false,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
+//Get all airlines
 router.get("/", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query("SELECT * FROM airlines_table");
+    const result = await client.query("SELECT * FROM airlines_table ORDER BY id ASC");
     const results = { results: result ? result.rows : null };
     res.json(results);
     client.release();
@@ -22,4 +23,48 @@ router.get("/", async (req, res) => {
   }
 });
 
+//Adding airlines
+router.post("/", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    await client.query(
+      `INSERT INTO airlines_table (name, country) VALUES ('${req.body.name}','${JSON.stringify(req.body.country)}');`
+    );
+    res.send("Added new airline")
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+//Update airlines
+router.put("/:id", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    await client.query(
+      `UPDATE airlines_table SET name = '${req.body.name}', country = '${JSON.stringify(req.body.country)}' WHERE id = ${req.params.id};`
+    );
+    res.send("Updated airline")
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+//Delete airlines
+router.delete("/:id", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    await client.query(
+      `DELETE FROM airlines_table WHERE id = ${req.params.id};`
+    );
+    res.send("Deleted airline")
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
 module.exports = router;
